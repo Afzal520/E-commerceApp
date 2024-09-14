ca13ba05029fa63ba7a7ca116a9123b7a930e530
Afzal520 <159987995+Afzal520@users.noreply.github.com>
2024-09-14 15:46:08 +0530

    change url

--- client/src/store/admin/order-slice/index.js
+++ client/src/store/admin/order-slice/index.js
@@ -11,7 +11,7 @@ export const getAllOrdersForAdmin = createAsyncThunk(
   "/order/getAllOrdersForAdmin",
   async () => {
     const response = await axios.get(
-      `https://e-commerceapp-qlpp.onrender.com/api/admin/orders/get`
+      `https://e-commerceapp-v9py.onrender.com/api/admin/orders/get`
     );
 
     return response.data;
@@ -22,7 +22,7 @@ export const getOrderDetailsForAdmin = createAsyncThunk(
   "/order/getOrderDetailsForAdmin",
   async (id) => {
     const response = await axios.get(
-      `https://e-commerceapp-qlpp.onrender.com/api/admin/orders/details/${id}`
+      `https://e-commerceapp-v9py.onrender.com/api/admin/orders/details/${id}`
     );
 
     return response.data;
@@ -33,7 +33,7 @@ export const updateOrderStatus = createAsyncThunk(
   "/order/updateOrderStatus",
   async ({ id, orderStatus }) => {
     const response = await axios.put(
-      `https://e-commerceapp-qlpp.onrender.com/api/admin/orders/update/${id}`,
+      `https://e-commerceapp-v9py.onrender.com/api/admin/orders/update/${id}`,
       {
         orderStatus,
       }
